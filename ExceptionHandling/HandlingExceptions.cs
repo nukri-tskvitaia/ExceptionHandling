4c11a5d429f64373a248b4bf2e9133f5a6c91db6
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-12-24 11:27:20 +0400

    Add project files.

---
+++ ExceptionHandling/HandlingExceptions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    public static class HandlingExceptions
+    {
+        public static bool CatchException(object obj)
+        {
+            try
+            {
+                ThrowException(obj);
+                return false;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+
+        public static bool CatchArgumentNullException(object obj, out string exceptionMessage)
+        {
+            exceptionMessage = string.Empty;
+
+            try
+            {
+                ThrowException(obj);
+                return false;
+            }
+            catch (ArgumentNullException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+        }
+
+        public static bool CatchArgumentException(int i, out string exceptionMessage)
+        {
+            exceptionMessage = string.Empty;
+
+            try
+            {
+                ThrowException(new object(), i);
+                return false;
+            }
+            catch (ArgumentException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+        }
+
+        public static bool CatchArgumentOutOfRangeException(int j, out string exceptionMessage)
+        {
+            exceptionMessage = string.Empty;
+
+            try
+            {
+                ThrowException(new object(), 1, j);
+                return false;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+        }
+
+        public static bool CatchExceptions(object obj, int i, int j, bool throwException, out string exceptionMessage)
+        {
+            exceptionMessage = string.Empty;
+
+            try
+            {
+                ThrowException(obj, i, j, throwException);
+                return false;
+            }
+            catch (ArgumentNullException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (ArgumentException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+        }
+
+        private static void ThrowException(object obj, int i = 1, int j = 1, bool throwException = false)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj), "obj is null.");
+            }
+
+            if (i == 0)
+            {
+                throw new ArgumentException("i parameter is invalid.", nameof(i));
+            }
+
+            if (j < 0 || j > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(j), "j is out of range.");
+            }
+
+            if (throwException)
+            {
+#pragma warning disable CA2201 // Do not raise reserved exception types
+                throw new Exception("exception is thrown.");
+#pragma warning restore CA2201 // Do not raise reserved exception types
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/ThrowingExceptions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace ExceptionHandling
+{
+    public static class ThrowingExceptions
+    {
+        public static void CheckParameterAndThrowException(object obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+        }
+
+        public static void CheckBothParametersAndThrowException(object obj1, object obj2)
+        {
+            if (obj1 is null || obj2 is null)
+            {
+                throw new ArgumentNullException(obj1 is null ? nameof(obj1) : nameof(obj2));
+            }
+        }
+
+        public static string CheckStringAndThrowException(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return str;
+        }
+
+        public static string CheckBothStringsAndThrowException(string str1, string str2)
+        {
+            if (str1 is null || str2 is null)
+            {
+                throw new ArgumentNullException(str1 is null ? nameof(str1) : nameof(str2));
+            }
+
+            return string.Concat(str1, str2);
+        }
+
+        public static int CheckEvenNumberAndThrowException(int evenNumber)
+        {
+            if (evenNumber % 2 != 0)
+            {
+                throw new ArgumentException($"{nameof(evenNumber)} is not even!");
+            }
+
+            return evenNumber;
+        }
+
+        public static int CheckCandidateAgeAndThrowException(int candidateAge, bool isCandidateWoman)
+        {
+            if (!isCandidateWoman && (candidateAge < 16 || candidateAge > 63))
+            {
+                throw new ArgumentOutOfRangeException(nameof(candidateAge));
+            }
+
+            if (isCandidateWoman && (candidateAge < 16 || candidateAge > 58))
+            {
+                throw new ArgumentOutOfRangeException(nameof(candidateAge));
+            }
+
+            return candidateAge;
+        }
+
+        public static string GenerateUserCode(int day, int month, string username)
+        {
+            if (day > 31 || day < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(day));
+            }
+
+            if (month > 12 || month < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(month));
+            }
+
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+
+            return $"{username}-{day}{month}";
+        }
+    }
+}
